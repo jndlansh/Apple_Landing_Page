c913454370ecf26bc0fcf009ee31c2992fbe6aa1
Ansh <jndl.ansh@gmail.com>
2025-11-03 14:11:46 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+const navLinks = [
+    { label: "Store" },
+    { label: "Mac" },
+    { label: "iPhone" },
+    { label: "Watch" },
+    { label: "Vision" },
+    { label: "AirPods" },
+];
+
+export {
+    navLinks
+};
